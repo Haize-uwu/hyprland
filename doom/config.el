bd90970b4d37fc2af982234e9f40cb8df376418a
Haize <84086558+Haize-uwu@users.noreply.github.com>
2024-04-05 15:55:16 +0200

    Add files via upload

--- doom/config.el
+++ doom/config.el
@@ -21,8 +21,8 @@
 ;; See 'C-h v doom-font' for documentation and more examples of what they
 ;; accept. For example:
 ;;
-;;(setq doom-font (font-spec :family "Fira Code" :size 12 :weight 'semi-light)
-;;      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
+(setq doom-font (font-spec :family "Source Code Pro" :size 15 :weight 'semi-light)
+      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 15))
 ;;
 ;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
 ;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
@@ -32,7 +32,8 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-moonlight)
+(setq doom-theme 'doom-laserwave)
+;;doom-ephemeral
 ;;doom-moonlight
 ;;(setq doom-theme 'doom-flatwhite)
 
@@ -40,12 +41,14 @@
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
 (setq display-line-numbers-type t)
-
+(add-to-list 'default-frame-alist '(alpha . 50))
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/org/")
 (require 'ccls)
 (setq ccls-sem-highlight-method 'font-lock)
+(setq corfu-auto-delay 0.5)
+;;(add-to-list 'default-frame-alist '(alpha-background . 96))
 
 ;; Whenever you reconfigure a package, make sure to wrap your config in an
 ;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
