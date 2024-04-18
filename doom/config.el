1238e537886e423849ac66764f547993fc61a76f
Haize <84086558+Haize-uwu@users.noreply.github.com>
2024-04-18 15:37:09 +0200

    Add files via upload

--- doom/config.el
+++ doom/config.el
@@ -21,7 +21,7 @@
 ;; See 'C-h v doom-font' for documentation and more examples of what they
 ;; accept. For example:
 ;;
-(setq doom-font (font-spec :family "Source Code Pro" :size 15 :weight 'semi-light)
+(setq doom-font (font-spec :family "Source Code Pro" :size 17 :weight 'semi-light)
       doom-variable-pitch-font (font-spec :family "Fira Sans" :size 15))
 ;;
 ;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
@@ -32,12 +32,14 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-laserwave)
+(setq doom-theme 'doom-moonlight)
+;;doom-laserwave
 ;;doom-ephemeral
 ;;doom-moonlight
 ;;(setq doom-theme 'doom-flatwhite)
 
-
+;;Latex
+(setq +latex-viewers '(pdf-tools))
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
 (setq display-line-numbers-type t)
